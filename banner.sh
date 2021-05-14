9d342750a54a7d1e950dd3f379f5149456703a6b
alegra8611 <76691298+alegra8611@users.noreply.github.com>
2021-05-14 12:21:37 +1000

    Update banner.sh

--- banner.sh
+++ banner.sh
@@ -17,7 +17,7 @@ echo -e ${BWHITE} "    MMMMMMMMMMMMMMMMMMMMMMMMMMMMMM                           
 echo -e ${BWHITE} "    MMMMMMMMMMMMMMMMMMMMMMMMMMMMMM                                                               MMMMMMMMMMMMMMMMMMMMMMMMMMMMMM"    
 echo -e ${BWHITE} "    MM+  MMMMMMMMMMMMMMMMMMMM$  MM  -----------------------------------------------------------  MM+  MMMMMMMMMMMMMMMMMMMM$  MM"
 echo -e ${BWHITE} "    +M.    ZMMMMMMMMMMMMMMO     M                                                                +M.    ZMMMMMMMMMMMMMMO     M"
-echo -e ${BWHITE} "     MM       MMMMMMMMMM       MM ____            _      ____              _     _               MM       MMMMMMMMMM       MM"
+echo -e ${BWHITE} "     MM       MMMMMMMMMM       MM ____            _      ____              _     _               MM       MMMMMMMMMM         MM"
 echo -e ${BWHITE} "    MMD         MMMM.        .MM |  _ \ ___  _ __| |_   |  _ \ _   _ _ __ (_)___| |__   ___ _ __  MMD         MMMM.        .MM"
 echo -e ${BWHITE} "     MMMMMM    MMM=MMI   DMMMMM  | |_) / _ \| '__| __|  | |_) | | | | '_ \| / __| '_ \ / _ \ '__|  MMMMMM    MMM=MMI   DMMMMM"
 echo -e ${BWHITE} "      MMMMMMMMMMM  +MMMMMMMMMMM  |  __/ (_) | |  | |_   |  __/| |_| | | | | \__ \ | | |  __/ |     MMMMMMMMMMM  +MMMMMMMMMMM"
